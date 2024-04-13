1796cdefd1997f8def2f9214ab7131e63c6f7973
Brad Brown <bradbrown@bradbrown.llc>
2024-04-12 23:25:41 -0400

    optional signal for query

--- lib/query.ts
+++ lib/query.ts
@@ -1,7 +1,7 @@
 import { Chain } from './types/chain.ts'
 import * as schemas from './schemas/mod.ts'
-export async function query(chainId:number):Promise<Error|Chain> {
-  return await fetch(`https://cdn.jsdelivr.net/gh/ethereum-lists/chains/_data/chains/eip155-${chainId}.json`)
+export async function query(chainId:number, { signal }:{ signal?:AbortSignal }={}):Promise<Error|Chain> {
+  return await fetch(`https://cdn.jsdelivr.net/gh/ethereum-lists/chains/_data/chains/eip155-${chainId}.json`, { signal })
     .then(response => response.json())
     .then(schemas.chain.parseAsync)
     .catch(reason => new Error(reason))
